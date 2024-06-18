8c1cd327f1dd8829289bd5d31a8baffdec9dab88
FestusMike <festusmike98@gmail.com>
2024-06-18 18:49:17 +0100

    ...

--- controller/settings.py
+++ controller/settings.py
@@ -104,17 +104,6 @@ DATABASES = {
     }
 }
 
-import sys
-if 'test' in sys.argv:
-    DATABASES['default']= {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'testdb',
-        'USER': 'root',
-        'PASSWORD': 'Timi1234',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
