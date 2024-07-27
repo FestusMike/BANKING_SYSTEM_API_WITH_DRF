0255e76f92c0068551bbbb58551902007c8c17c6
FestusMike <festusmike98@gmail.com>
2024-07-27 07:40:59 +0100

    minor stuff here and there

--- controller/urls.py
+++ controller/urls.py
@@ -25,10 +25,10 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("control-panel", admin.site.urls),
-    path("schema", SpectacularAPIView.as_view(), name="schema"),
-    path("docs", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/v1/schema", SpectacularAPIView.as_view(), name="schema"),
+    path("api/v1/docs", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    path("redoc", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("api/v1/redoc", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     path("api/v1/auth/", include("accounts.urls")),
     path("api/v1/", include("banking.urls")),
     path("api/v1/", include("messaging.urls")),
